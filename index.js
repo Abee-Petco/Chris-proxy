3349fc883ba2ae3ddd4c8859d76d369d203a7a7f
Chris Hesterman <chrishesterman14@gmail.com>
2020-09-22 10:35:09 -0700

    Implement K6, new relic testing software.

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+// require('newrelic');
 const express = require('express');
 const serveStatic = require('serve-static');
-const morgan = require('morgan');
 
-const server = express();
+const app = express();
 
-server.use(morgan('dev'));
-server.use(serveStatic('./client/'));
+app.use(serveStatic('./client/'));
 
-server.get('/product', (req, res) => {
+app.get('/product', (req, res) => {
   const { itemID } = req.query;
   const itemIdNumber = Number.parseInt(itemID, 10);
 
-  if (itemIdNumber < 100 || itemIdNumber > 199 || itemIdNumber === undefined) {
+  if (itemIdNumber < 100 || itemIdNumber > 10000100 || itemIdNumber === undefined) {
     res.status(404).send('itemID invalid');
   } else {
-    res.sendFile(`${__dirname}/client/index.html`);
+    res.sendFile(`${__dirname}/client/index.html`, (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.end();
+      }
+    });
   }
 });
 
-server.listen(3000);
+app.listen(3000);
